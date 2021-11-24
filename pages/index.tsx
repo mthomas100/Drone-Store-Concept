779d21cba531c56718a427ab9d6c599b58398d62
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-11-24 13:30:48 -0500

    cleared conflict index

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,7 @@ import {
 } from "components";
 import createApolloClient from "@services/graphql";
 import {
-<<<<<<< HEAD
     Hero,
-=======
->>>>>>> 799a5481abedd6ece0ad7673601722c2ddbe48e9
     MarketingCard,
     Product,
     SellingPoint,
@@ -28,10 +25,7 @@ import {
     PRODUCTS_QUERY,
     SELLING_POINT_QUERY,
     TESTIMONIALS_QUERY,
-<<<<<<< HEAD
     HERO_QUERY,
-=======
->>>>>>> 799a5481abedd6ece0ad7673601722c2ddbe48e9
 } from "graphql/queries";
 
 type HomeProps = {
@@ -39,10 +33,7 @@ type HomeProps = {
     products: Product[];
     sellingPoint: SellingPoint;
     testimonials: Testimonial[];
-<<<<<<< HEAD
     hero: Hero;
-=======
->>>>>>> 799a5481abedd6ece0ad7673601722c2ddbe48e9
 };
 
 const Home: React.FC<HomeProps> = ({
@@ -50,20 +41,13 @@ const Home: React.FC<HomeProps> = ({
     products,
     sellingPoint,
     testimonials,
-<<<<<<< HEAD
     hero,
-=======
->>>>>>> 799a5481abedd6ece0ad7673601722c2ddbe48e9
 }) => {
     return (
         <Container>
             <MobileNav />
             <Header />
-<<<<<<< HEAD
             <Main hero={hero} />
-=======
-            <Main />
->>>>>>> 799a5481abedd6ece0ad7673601722c2ddbe48e9
             <MarketingCards marketingCards={marketingCards} />
             <SellingPoints sellingPoint={sellingPoint} />
             <NewProducts products={products} />
@@ -93,13 +77,10 @@ export const getStaticProps: GetStaticProps = async () => {
     const { data: { testimonials }} = await createApolloClient.query({
         query: TESTIMONIALS_QUERY,
     });
-<<<<<<< HEAD
 
     const { data: { hero }} = await createApolloClient.query({
         query: HERO_QUERY,
     });
-=======
->>>>>>> 799a5481abedd6ece0ad7673601722c2ddbe48e9
     /* eslint-enable */
 
     return {
@@ -108,10 +89,7 @@ export const getStaticProps: GetStaticProps = async () => {
             products,
             sellingPoint,
             testimonials,
-<<<<<<< HEAD
             hero,
-=======
->>>>>>> 799a5481abedd6ece0ad7673601722c2ddbe48e9
         },
     };
 };
